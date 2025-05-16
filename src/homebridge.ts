ed57ea19924fcfb5ffc4eecb9e77705b61469b40
Michael Ahern <michael.ahern@gmail.com>
2025-05-16 22:12:01 +0000

    Refactor

--- src/homebridge.ts
+++ src/homebridge.ts
@@ -1,14 +1,14 @@
 import { API, Characteristic, Formats, Perms, WithUUID } from 'homebridge';
 
 export class HomebridgeExtensions {
-    private static readonly AMPS_NAME = 'Amps';
-    private static readonly AMPS_UUID = 'E863F126-079E-48FF-8F27-9C2605A29F52';
+    static readonly #AMPS_NAME = 'Amps';
+    static readonly #AMPS_UUID = 'E863F126-079E-48FF-8F27-9C2605A29F52';
 
-    private static readonly VOLTS_NAME = 'Volts';
-    private static readonly VOLTS_UUID = 'E863F10A-079E-48FF-8F27-9C2605A29F52';
+    static readonly #VOLTS_NAME = 'Volts';
+    static readonly #VOLTS_UUID = 'E863F10A-079E-48FF-8F27-9C2605A29F52';
 
-    private static readonly WATTS_NAME = 'Watts';
-    private static readonly WATTS_UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
+    static readonly #WATTS_NAME = 'Watts';
+    static readonly #WATTS_UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
 
     private readonly api: API;
 
@@ -17,11 +17,11 @@ export class HomebridgeExtensions {
     constructor(api: API) {
         this.api = api;
 
-        this.Characteristic[HomebridgeExtensions.AMPS_NAME] = class extends this.api.hap.Characteristic {
-            static readonly UUID: string = HomebridgeExtensions.AMPS_UUID;
+        this.Characteristic[HomebridgeExtensions.#AMPS_NAME] = class extends this.api.hap.Characteristic {
+            static readonly UUID: string = HomebridgeExtensions.#AMPS_UUID;
 
             constructor() {
-                super(HomebridgeExtensions.AMPS_NAME, HomebridgeExtensions.AMPS_UUID, {
+                super(HomebridgeExtensions.#AMPS_NAME, HomebridgeExtensions.#AMPS_UUID, {
                     format: Formats.FLOAT,
                     perms: [Perms.NOTIFY, Perms.PAIRED_READ],
                     unit: 'A',
@@ -33,11 +33,11 @@ export class HomebridgeExtensions {
             }
         };
 
-        this.Characteristic[HomebridgeExtensions.VOLTS_NAME] = class extends this.api.hap.Characteristic {
-            static readonly UUID: string = HomebridgeExtensions.VOLTS_UUID;
+        this.Characteristic[HomebridgeExtensions.#VOLTS_NAME] = class extends this.api.hap.Characteristic {
+            static readonly UUID: string = HomebridgeExtensions.#VOLTS_UUID;
 
             constructor() {
-                super(HomebridgeExtensions.VOLTS_NAME, HomebridgeExtensions.VOLTS_UUID, {
+                super(HomebridgeExtensions.#VOLTS_NAME, HomebridgeExtensions.#VOLTS_UUID, {
                     format: Formats.FLOAT,
                     perms: [Perms.NOTIFY, Perms.PAIRED_READ],
                     unit: 'V',
@@ -49,11 +49,11 @@ export class HomebridgeExtensions {
             }
         };
 
-        this.Characteristic[HomebridgeExtensions.WATTS_NAME] = class extends this.api.hap.Characteristic {
-            static readonly UUID: string = HomebridgeExtensions.WATTS_UUID;
+        this.Characteristic[HomebridgeExtensions.#WATTS_NAME] = class extends this.api.hap.Characteristic {
+            static readonly UUID: string = HomebridgeExtensions.#WATTS_UUID;
 
             constructor() {
-                super(HomebridgeExtensions.WATTS_NAME, HomebridgeExtensions.WATTS_UUID, {
+                super(HomebridgeExtensions.#WATTS_NAME, HomebridgeExtensions.#WATTS_UUID, {
                     format: Formats.FLOAT,
                     perms: [Perms.NOTIFY, Perms.PAIRED_READ],
                     unit: 'W',
